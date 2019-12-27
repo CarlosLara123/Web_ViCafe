74886c001622f338972c844279826e445ffbd71a
CarlosLara123 <carlosdaniellarsol@gmail.com>
2019-12-27 12:04:43 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+const cors = require('cors'); 
 const bodyparser = require("body-parser");
 
 //CARGAR RUTAS
@@ -12,15 +13,16 @@ var farmer_routes = require('./routes/farmer.routes');
 // var message_routes = require('./routes/messageRoutes');
 
 //MIDDELWARES
+app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false}));
 app.use(bodyparser.json());
 
 //CABEZERAS
 app.use((req, res, next) =>{
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE'); 
 
     next();
 });
